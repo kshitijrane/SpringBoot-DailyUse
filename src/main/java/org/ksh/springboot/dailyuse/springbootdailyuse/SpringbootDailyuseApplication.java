210285e7b2bc8b6d57cf183d0b6bc15cafb8b2ea
Kshitij Rane <krane28@hotmail.com>
2021-12-17 17:35:55 +0530

    Initial Code commit

---
+++ src/main/java/org/ksh/springboot/dailyuse/springbootdailyuse/SpringbootDailyuseApplication.java
@@ -0,0 +1,13 @@
+package org.ksh.springboot.dailyuse.springbootdailyuse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDailyuseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDailyuseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ksh/springboot/dailyuse/springbootdailyuse/SpringbootDailyuseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ksh.springboot.dailyuse.springbootdailyuse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDailyuseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
